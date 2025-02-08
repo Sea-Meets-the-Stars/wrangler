c6157298c5726970a5f4d4109ae89c5489412935
profxj <xavier@ucolick.org>
2025-02-07 22:03:44 -0800

    tst

--- wrangler/tests/test_extract.py
+++ wrangler/tests/test_extract.py
@@ -12,12 +12,12 @@ def test_grab_filelist():
                                  verbose=True, skip_download=True))
 
 # Extract
-def test_extract():
-    extract_options = dict(field_size=(192,192))
-    asyncio.run(grab_and_go.run('VIIRS_NPP', '2024-01-01', 
-                                '2024-01-02', extract_options, 
-                                'test.h5', 
-                                verbose=True, debug=False, 
-                                save_local_files=False))
+#def test_extract():
+extract_options = dict(field_size=(192,192))
+asyncio.run(grab_and_go.run('VIIRS_NPP', '2024-01-01', 
+                            '2024-01-02', extract_options, 
+                            'test.h5', 
+                            verbose=True, debug=False, 
+                            save_local_files=False))
 
     
