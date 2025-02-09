706b5b7343a9a74c556b3cf03d61736738746616
profxj <xavier@ucolick.org>
2025-02-08 19:22:11 -0800

    test

--- wrangler/tests/test_extract.py
+++ wrangler/tests/test_extract.py
@@ -13,11 +13,11 @@ def test_grab_filelist():
 
 # Extract
 #def test_extract():
-extract_options = dict(field_size=(192,192))
+extract_file = 'files/extract_viirs_std.json'
 asyncio.run(grab_and_go.run('VIIRS_NPP', '2024-01-01', 
-                            '2024-01-02', extract_options, 
+                            '2024-01-02', extract_file,
                             'test.h5', 
                             verbose=True, debug=False, 
-                            save_local_files=False))
+                            save_local_files=True))
 
     
