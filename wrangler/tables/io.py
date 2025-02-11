badd23d5c217e004e86d9415ec020740928e273b
profxj <xavier@ucolick.org>
2025-02-11 15:25:29 -0800

    tbl

--- wrangler/tables/io.py
+++ wrangler/tables/io.py
@@ -5,6 +5,57 @@ import pandas
 
 from wrangler import io as wrangler_io
 
+
+def load_main_table(tbl_file:str, verbose=True):
+    """Load the table of cutouts 
+
+    Args:
+        tbl_file (str): Path to table of cutouts. Local or s3
+        verbose (bool, optional): [description]. Defaults to True.
+
+    Raises:
+        IOError: [description]
+
+    Returns:
+        pandas.DataFrame: table of cutouts
+    """
+    _, file_extension = os.path.splitext(tbl_file)
+
+    # s3?
+    if tbl_file[0:5] == 's3://':
+        inp = wrangler_io.load_to_bytes(tbl_file)
+    else:
+        inp = tbl_file
+        
+    # Allow for various formats
+    if file_extension == '.csv':
+        main_table = pandas.read_csv(inp, index_col=0)
+        # Set time
+        if 'datetime' in main_table.keys():
+            main_table.datetime = pandas.to_datetime(main_table.datetime)
+    elif file_extension == '.feather':
+        # Allow for s3
+        main_table = pandas.read_feather(inp)
+    elif file_extension == '.parquet':
+        # Allow for s3
+        main_table = pandas.read_parquet(inp)
+    else:
+        raise IOError("Bad table extension: ")
+
+    # Deal with masked int columns
+    for key in ['gradb_Npos', 'FS_Npos', 'UID', 'pp_type']:
+        if key in main_table.keys():
+            main_table[key] = pandas.array(main_table[key].values, dtype='Int64')
+    # Report
+    if verbose:
+        print("Read main table: {}".format(tbl_file))
+
+    # Decorate
+    if 'DT' not in main_table.keys() and 'T90' in main_table.keys():
+        main_table['DT'] = main_table.T90 - main_table.T10
+        
+    return main_table
+
 def write_main_table(main_table:pandas.DataFrame, outfile:str, to_s3=False):
     """Write Main table for ULMO analysis
     Format is determined from the outfile extension.
@@ -32,3 +83,4 @@ def write_main_table(main_table:pandas.DataFrame, outfile:str, to_s3=False):
     else:
         raise IOError("Not ready for this")
     print("Wrote Analysis Table: {}".format(outfile))
+
