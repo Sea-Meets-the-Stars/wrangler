7899864367d0c705c06ad96bed0f99ff33e78826
profxj <xavier@ucolick.org>
2025-10-08 14:10:55 -0700

    cbar kws

--- wrangler/plotting/cutout.py
+++ wrangler/plotting/cutout.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 def show_image(img:np.ndarray, cm=None, cbar:bool=True, 
                vmnx=(None,None), show=False, set_aspect=None, clbl=None,
-               ax=None):
+               ax=None, cb_kws:dict=None):
     """Dispay the cutout image
     Args:
         img (np.ndarray): cutout image
@@ -20,16 +20,21 @@ def show_image(img:np.ndarray, cm=None, cbar:bool=True,
         set_aspect (str, optional):
             Passed to ax.set_aspect() if provided
         ax (matplotlib.Axis, optional): axis to use for the plot
+        cb_kws (dict, optional): Additional keywords for the color bar
     Returns:
         matplotlib.Axis: axis containing the plot
     """
     if cm is None:
         cm = 'jet'
-    #
+    # Color bar
+    cbar_kws={'label': clbl}
+    if cb_kws is not None:
+        cbar_kws.update(cb_kws)
+    # Do it
     ax = sns.heatmap(np.flipud(img), xticklabels=[], 
                      vmin=vmnx[0], vmax=vmnx[1], ax=ax,
                      yticklabels=[], cmap=cm, cbar=cbar, 
-                     cbar_kws={'label': clbl})# 'fontsize': 20})
+                     cbar_kws=cbar_kws)
     #plt.savefig('image', dpi=600)
     
     if show:
